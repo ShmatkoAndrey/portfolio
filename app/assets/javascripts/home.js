635be4d60d822ba54dec546c0e119324ebe7dd9f
Andrey Shmatko <shmatuan@gmail.com>
2016-03-15 17:03:55 +0200

    render partials

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     $('.fullpage').fullpage({
-        menu: '.navbar',
-        verticalCentered: true,
+        //menu: '.navbar',
+        //verticalCentered: false,
         resize : false,
-        anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-        navigation: true,
-        navigationPosition: 'right',
-        navigationTooltips:['firstPageTooltip', 'secondPageTooltip', 'thirdPageTooltip', 'fourthPageTooltip'],
+        //anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+        //navigation: true,
+        //navigationPosition: 'right',
+        //navigationTooltips:['firstPageTooltip', 'secondPageTooltip', 'thirdPageTooltip', 'fourthPageTooltip'],
         css3: true
     });
 });
