cf4ffc95784b66551d3b11ca2cac29c59890fb94
Andrey Shmatko <shmatuan@gmail.com>
2016-03-17 15:39:14 +0200

    devise some js

--- app/assets/javascripts/skillclick.js
+++ app/assets/javascripts/skillclick.js
@@ -1,6 +1,6 @@
 function skillclick(skill) {
     $('#skills_list').find('li').each(function(i, e){e.className = ''});
     $('#li_skill_' + skill)[0].className += 'active';
-    $('#all_skills').find('div').each(function(i, e){e.style.display = 'none'});
+    $('#all_skills').find('.skill_show_block').each(function(i, e){e.style.display = 'none'});
     $('#skill_' + skill)[0].style.display = 'block'
 }
