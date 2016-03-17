86b0b26ead90d86b55c23b4de641f9388e826aaf
Andrey Shmatko <shmatuan@gmail.com>
2016-03-17 17:22:36 +0200

    del trash code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,4 @@
 class Users::SessionsController < Devise::SessionsController
-prepend_before_filter :verify_user, only: [:destroy]
 skip_before_filter :verify_authenticity_token, :only => [:destroy, :create]
 # before_filter :configure_sign_in_params, only: [:create]
 
@@ -29,14 +28,6 @@ skip_before_filter :verify_authenticity_token, :only => [:destroy, :create]
 
     respond_to do |format|
       format.js {}
-      # format.html {redirect_to :back}
-    end
-  end
-
-  def log_me_out
-    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-    respond_to do |format|
-      format.js
     end
   end
 
@@ -46,11 +37,4 @@ skip_before_filter :verify_authenticity_token, :only => [:destroy, :create]
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.for(:sign_in) << :attribute
   # end
-  private
-
-  def verify_user
-    ## redirect to appropriate path
-    redirect_to new_user_session_path, notice: 'You have already signed out. Please sign in again.' and return unless user_signed_in?
-  end
-
 end
