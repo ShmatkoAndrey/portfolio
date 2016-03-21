b6c4bb7f03933df2ac84afc68d3d6db0994d4374
Andrey Shmatko <shmatuan@gmail.com>
2016-03-21 12:03:40 +0200

    some pics fix

--- app/assets/javascripts/dropzone_files.js
+++ app/assets/javascripts/dropzone_files.js
@@ -13,32 +13,11 @@ $(function() {
 });
 
 var appendContent = function(imageUrl, mediaId) {
-    $("#media-contents").prepend('<div class="thumbnail" style="border: 1px solid red;"><img src="' + imageUrl + '"/></div>');
+    $("#media-contents").prepend('<div class="thumbnail" style="border: 1px solid red;" onClick="showImage(\''+imageUrl+'\')">' +
+        '<img src="' + imageUrl + '"/></div>');
 };
 
 function showImage(url){
     var code = '<img src="' + url + '" />';
     $('#image_show').html(code);
 }
-
-
-//$('#myModal').modal({
-//    show: true,
-//    keyboard: true,
-//    backdrop: false
-//});
-//
-//$('#myModal').on('shown.bs.modal', function(e) {
-//    var $invoker, code;
-//    $('#myInput').focus();
-//    $invoker = $(e.relatedTarget);
-//    code = '<button aria-label="Close" class="close" data-dismiss="modal" type="button"> <span aria-hidden="true">×</span> </button> <br />';
-//    code += '<img src ="' + $invoker[0].src + '"</img>';
-//    $('.modal-body').html(code);
-//    $('.modal-backdrop').remove();
-//
-//});
-//
-//$('#myModal').on('hidden.bs.modal', function() {
-//    $('.modal-body').html('');
-//});
