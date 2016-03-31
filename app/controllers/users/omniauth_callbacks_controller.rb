9173c143a5314ec2e6a0e091cc87ed2242cc9283
Andrey Shmatko <shmatuan@gmail.com>
2016-03-31 11:38:49 +0300

    redirect to slide

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,17 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def self.provides_callback_for(provider)
-      class_eval %Q{
+  def self.provides_callback_for(provider)
+    class_eval %Q{
       def #{provider}
         @user = User.find_for_oauth(env["omniauth.auth"], current_user)
-
         if @user.persisted?
           sign_in @user#, root_path #sign_in_and_redirect
-          redirect_to root_path
+          redirect_to root_path + "/#firstPage/slide2"
         else
           session["devise.#{provider}_data"] = env["omniauth.auth"]
         end
       end
     }
-    end
+  end
 
   [:twitter, :facebook].each do |provider|
     provides_callback_for provider
